d8942c54e2df59309f8ceb0feb570be21b287078
Daniel Neto <me@danielneto.com>
2026-04-02 15:01:18 -0300

    fix: Ensure admin access before executing FFMPEG remote test

--- plugin/CloneSite/client.log.php
+++ plugin/CloneSite/client.log.php
@@ -1,2 +1,8 @@
 <?php
+require_once '../../videos/configuration.php';
+if (!User::isAdmin()) {
+    $obj = new stdClass();
+    $obj->msg = "You can't do this";
+    die(json_encode($obj));
+}
 include '../../videos/cache/clones/client.log';
