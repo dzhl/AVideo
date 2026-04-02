713509b4a95c007e8352c514bd8636305a80fe76
Daniel Neto <me@danielneto.com>
2026-04-02 14:58:53 -0300

    fix: Add admin check before processing FFMPEG remote test
    https://github.com/WWBN/AVideo/security/advisories/GHSA-2vg4-rrx4-qcpq

--- plugin/API/check.ffmpeg.json.php
+++ plugin/API/check.ffmpeg.json.php
@@ -3,6 +3,10 @@ $configFile = __DIR__.'/../../videos/configuration.php';
 require_once $configFile;
 header('Content-Type: application/json');
 
+if (!User::isAdmin()) {
+    forbiddenPage('Must be admin');
+}
+
 $obj = testFFMPEGRemote();
 
 die(json_encode($obj));
